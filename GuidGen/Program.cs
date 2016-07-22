f6b6d01c05fb4112b8758eaed5d0fae84422b560
Edward Blacklee <edward.blacklee@advancedcomputersoftware.com>
2016-07-22 17:13:59 +0100

    initial commit

---
+++ GuidGen/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GuidGen
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape)
+            {
+                var guid = Guid.NewGuid();
+                Console.WriteLine(guid);
+                Thread thread = new Thread(() => Clipboard.SetText(guid.ToString()));
+                thread.SetApartmentState(ApartmentState.STA); //Set the thread to STA
+                thread.Start();
+                thread.Join();
+            }
+        }
+    }
+}
